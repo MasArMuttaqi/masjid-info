00078f87125bebae51982ae4964e00cd9864307e
MasArMuttaqi <140738717+MasArMuttaqi@users.noreply.github.com>
2025-08-14 13:28:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -83,6 +83,133 @@ function updateClockEverySecond() {
 updateClockEverySecond();
 // jam otomatis
 
+
+$(function(){
+
+    // Cache some selectors
+
+    var clock = $('#clock'),
+        ampm = clock.find('.ampm');
+
+    // Map digits to their names (this will be an array)
+    var digit_to_name = 'zero one two three four five six seven eight nine'.split(' ');
+
+    // This object will hold the digit elements
+    var digits = {};
+
+    // Positions for the hours, minutes, and seconds
+    var positions = [
+        'h1', 'h2', ':', 'm1', 'm2', ':', 's1', 's2'
+    ];
+
+    // Generate the digits with the needed markup,
+    // and add them to the clock
+
+    var digit_holder = clock.find('.digits');
+
+    $.each(positions, function(){
+
+        if(this == ':'){
+            digit_holder.append('<div class="dots">');
+        }
+        else{
+
+            var pos = $('<div>');
+
+            for(var i=1; i<8; i++){
+                pos.append('<span class="d' + i + '">');
+            }
+
+            // Set the digits as key:value pairs in the digits object
+            digits[this] = pos;
+
+            // Add the digit elements to the page
+            digit_holder.append(pos);
+        }
+
+    });
+
+    // Add the weekday names
+
+    var weekday_names = 'AHAD SENIN SELASA RABU KAMIS JUM&apos;AT SABTU'.split(' '),
+        weekday_holder = clock.find('.weekdays');
+
+    $.each(weekday_names, function(){
+        weekday_holder.append('<span>' + this + '</span>');
+    });
+
+    var weekdays = clock.find('.weekdays span');
+
+    // Run a timer every second and update the clock
+
+    // hari pasaran jawa
+        var hariPasaran = 'Legi Pahing Pon Wage Kliwon'.split(' '),
+            Haripasaran_holder = clock.find('.pasaran');
+
+        $.each(hariPasaran, function(){
+            Haripasaran_holder.append('<span>' + this + '</span>');
+        });
+
+        var pasaran = clock.find('.pasaran span');
+
+        function HariPasaranJawa(tanggalInput) {
+          const tanggal = new Date(tanggalInput);
+          const acuan = new Date('1900-01-01'); // Hitung selisih hari dari acuan (1 Januari 1900 adalah Legi)
+          const selisihHari = Math.floor((tanggal - acuan) / (1000 * 60 * 60 * 24));
+          const pasaran = selisihHari % 5;
+
+          return `${pasaran}`;
+        } 
+        // hari pasaran jawa
+
+    (function update_time(){
+
+        // Use moment.js to output the current time as a string
+        // hh is for the hours in 12-hour format,
+        // mm - minutes, ss-seconds (all with leading zeroes),
+        // d is for day of week and A is for AM/PM
+
+        var now = moment().format("hhmmssd");
+
+        digits.h1.attr('class', digit_to_name[now[0]]);
+        digits.h2.attr('class', digit_to_name[now[1]]);
+        digits.m1.attr('class', digit_to_name[now[2]]);
+        digits.m2.attr('class', digit_to_name[now[3]]);
+        digits.s1.attr('class', digit_to_name[now[4]]);
+        digits.s2.attr('class', digit_to_name[now[5]]);
+
+        // The library returns Sunday as the first day of the week.
+        // Stupid, I know. Lets shift all the days one position down, 
+        // and make Sunday last
+
+        var dow = now[6];
+        dow--;
+
+        // Sunday!
+        if(dow < 0){
+            // Make it last
+            dow = 6;
+        }
+
+        // Mark the active day of the week
+        weekdays.removeClass('active').eq(dow).addClass('active');
+
+        var hari = moment().format("YYYY-MM-DD");
+        var HariPasaran = HariPasaranJawa(hari);
+
+        pasaran.removeClass('active').eq(HariPasaran).addClass('active');
+
+
+        // Set the am/pm text:
+        ampm.text("WIB");
+
+        // Schedule this function to be run again in 1 sec
+        setTimeout(update_time, 1000);
+
+    })();
+
+});
+
 // melihat tanggal hijriah Set zona waktu Indonesia (WIB)
 moment.locale('id');
 const bulanHijriah = moment().utcOffset(7).format('iMMMM');
